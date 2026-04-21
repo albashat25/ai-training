8b26bb29ee673d3159e67c9f39092c51e763ec52
albashat25 <mosaalbashat12345@gmail.com>
2026-04-21 15:00:23 +0300

    Update trainer.js

--- trainer.js
+++ trainer.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const questionsFile = 'questions_batch11.json';
+const questionsFile = 'questions_batch12.json';
 if (!fs.existsSync(questionsFile)) {
   console.error(`❌ خطأ: الملف ${questionsFile} غير موجود!`);
   process.exit(1);
